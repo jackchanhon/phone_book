941568af2b7044a427157b4486f3d9ddacdc791e
Chan YoongHon <chanyoonghon@Chans-MacBook-Pro.local>
2020-10-11 12:25:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Vue Phone Book
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Open web application in browser
+```
+Open browser with following url: http://localhost:8080/ 
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="body">
+    <p class="header"><strong>{{title}}</strong></p>
+    <div class="form">
+      <div class="form-item">
+        <label>First Name:</label>
+        <br />
+        <input type="text" class="userFirstname" v-model="userFirstname"/>
+        <br />
+      </div>
+      <div class="form-item">
+        <label>Last Name:</label> 
+        <br />
+        <input type="text" class="userLastname"  v-model="userLastname"/>
+        <br />
+      </div>
+      <div class="form-item">
+        <label>Phone:</label>
+        <br />
+        <input type="number" class="userPhone"  v-model="userPhone"/>
+        <br />
+      </div>
+      <button class="submitButton" v-on:click="submit">{{submitButton}}</button>
+    </div>
+    <div class="table">
+      <p class="header"><strong>Contact List</strong></p>
+      <table id="phoneBookItems" class="informationTable">
+        <!-- items should go here -->
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+           <tbody>
+            <tr v-for="(phoneBookRow,i) in phoneBookRows" :key="i">
+              <td> {{phoneBookRow.firstName}} </td>
+              <td> {{phoneBookRow.lastName}}  </td>
+              <td> {{phoneBookRow.phoneNumber}}  </td>
+              <td> 
+                <button @click="editRow(phoneBookRow, i)">Edit</button>
+                <button @click="deleteRow(i)">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "App",
+    data() {
+      return {
+        selectedIndex: -1,
+        items: [],
+        title: 'Add a new contact:',
+        submitButton: 'Add New Contact',
+        userFirstname: '',
+        userLastname: '',
+        userPhone: '',
+        phoneBookRows:  [{
+                            'firstName': 'Haley',
+                            'lastName': 'Prichet',
+                            'phoneNumber': '019232323',
+                          },{
+                            'firstName': 'John',
+                            'lastName': 'Wick',
+                            'phoneNumber': '0195873473',
+                          },{
+                            'firstName': 'Micheal',
+                            'lastName': 'Jordan',
+                            'phoneNumber': '0195873471',
+                          },{
+                            'firstName': 'Son',
+                            'lastName': 'Goku',
+                            'phoneNumber': '0195873470',
+                          }]
+      }
+    },
+    methods: {
+      submit() {
+        const newContact = {
+          firstName: this.userFirstname,
+          lastName: this.userLastname,
+          phoneNumber: this.userPhone
+        }
+
+        if(this.submitButton === 'Edit Contact') {
+          this.title = 'Add a new contact:'
+          this.submitButton = 'Add New Contact'
+          this.phoneBookRows[this.selectedIndex] = newContact
+        } else {
+          this.phoneBookRows.push(newContact)
+        }
+
+        this.userFirstname = ''
+        this.userLastname = ''
+        this.userPhone = ''
+      },
+      editRow(editRowItem, index) {
+        this.title = 'Edit contact :'
+        this.submitButton = 'Edit Contact'
+        const selectedItem = { ...editRowItem }
+        this.selectedIndex = index
+        this.userFirstname = selectedItem.firstName
+        this.userLastname = selectedItem.lastName
+        this.userPhone = selectedItem.phoneNumber
+      },
+      deleteRow(item) {
+        this.phoneBookRows.splice(item, 1);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .body {
+    margin: 10px;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  table, th, td {
+    border: 1px solid black;
+  }
+
+  th {
+    height: 50px;
+  }
+  td {
+    height: 30px;
+    text-align: center;
+  }
+
+  .header {
+    font-size: 25px;
+    margin-left: 10px;
+    color: black;
+  }
+
+  label {
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  input {
+    width: 300px;
+    height: 30px;
+  }
+
+  .form-item {
+    margin: 20px 0;
+  }
+
+  .form {
+    margin: 20px 0;
+  }
+
+  .submitButton {
+    width: 200px;
+    height: 30px;
+    margin: 10px 0;
+  }
+
+</style>
